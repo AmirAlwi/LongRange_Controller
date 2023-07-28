e4f53abcd5cefb27899e9ca7e4bb12e2de8a4d40
Amirrullah Alwi <71260595+karakuve@users.noreply.github.com>
2023-07-28 20:41:00 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <HardwareSerial.h>
 
 HardwareSerial SerialPort(2); // use UART2
-
+uint8_t broadcastAddress[] = {0xC4,0x5B,0xBE,0x3F,0x4C,0xD8};
+esp_now_peer_info_t peerInfo;
 
 typedef struct struct_message {
   char data;
@@ -18,12 +19,18 @@ void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len) {
   SerialPort.write(mydata.data);
 }
 
+void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t _status)
+{
+	Serial.println(_status == ESP_NOW_SEND_SUCCESS ? "Send OK" : "Send Fail");
+}
+
 void setup(){
   Serial.begin(9600);
   SerialPort.begin(9600, SERIAL_8N1, 17, 16);
 
 
   WiFi.mode(WIFI_STA);
+	Serial.println(WiFi.macAddress());
 
   if(esp_now_init() != ESP_OK){
     Serial.println("error initialisation");
@@ -31,8 +38,23 @@ void setup(){
   }
 
   esp_now_register_recv_cb(OnDataRecv);
+  esp_now_register_send_cb(onDataSent);
+
+	memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+	peerInfo.channel = 0;
+	peerInfo.encrypt = false;
+
+	if (esp_now_add_peer(&peerInfo) != ESP_OK){
+		Serial.println("Failed to add peer");
+		return;
+	}
 }
 
+char input;
 void loop(){
-
+  if (Serial.available()){
+    input = char(Serial.read());
+    Serial.printf("recv %c \n", input);
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&input, sizeof(mydata)); 	
+  }
 }
